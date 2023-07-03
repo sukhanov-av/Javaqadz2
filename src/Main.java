1514b6ca2934ae80affa030acc40fc33e3728823
Sukhanov_AV <sukhanov_av@bk.ru>
2023-07-03 13:53:16 +0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int ticketPrice = 13676;
+
+        int bonusSum = 20;
+
+        int bonus = ticketPrice / bonusSum;
+        System.out.println("Java-авиа приветствует вас!");
+        System.out.println("Ваш итоговый бонус составляет " + bonus);
+        System.out.println("руб.");
+//int percent = isRegistered ? 3 : 1;
+    }
+}
